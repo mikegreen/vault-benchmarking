23499f5d299b89f02b6db682e08d274aab25bdf3
Jacob Friedman <jacobdavidfriedman@gmail.com>
2019-03-19 16:38:59 -0700

    Create write-random-secrets.lua

---
+++ write-random-secrets.lua
@@ -0,0 +1,67 @@
+-- Script that writes secrets to k/v engine in Vault
+-- You can specify the number of distinct secrets to write by adding "-- <N>" after the URL
+
+local counter = 1
+local threads = {}
+
+function setup(thread)
+   thread:set("id", counter)
+   table.insert(threads, thread)
+   counter = counter + 1
+end
+
+function init(args)
+   if args[1] == nil then
+      num_secrets = 1000
+   else
+      num_secrets = tonumber(args[1])
+   end
+   print("Number of secrets is: " .. num_secrets)
+   requests  = 0
+   writes = 0
+   responses = 0
+   method = "POST"
+   -- give each thread different random seed
+   math.randomseed(os.time() + id*1000)
+   local msg = "thread %d created"
+   print(msg:format(id))
+end
+
+function request()
+   writes = writes + 1
+   -- randomize path to secret
+   path = "/v1/secret/write-random-test-" .. math.random(num_secrets)
+   -- minimal secret giving thread id and # of write
+   -- body = '{"foo-' .. id .. '" : "bar-' .. writes ..'"}'
+   -- add extra key with 100 bytes
+   body = '{"thread-' .. id .. '" : "write-' .. writes ..'","extra" : "1xxxxxxxxx2xxxxxxxxx3xxxxxxxxx4xxxxxxxxx5xxxxxxxxx6xxxxxxxxx7xxxxxxxxx8xxxxxxxxx9xxxxxxxxx0xxxxxxxxx"}'
+   requests = requests + 1
+   return wrk.format(method, path, nil, body)
+end
+
+function response(status, headers, body)
+   responses = responses + 1
+end
+
+done = function(summary, latency, requests)
+   io.write("\nJSON Output:\n")
+   io.write("{\n")
+   io.write(string.format("\t\"requests\": %d,\n", summary.requests))
+   io.write(string.format("\t\"duration_in_microseconds\": %0.2f,\n", summary.duration))
+   io.write(string.format("\t\"bytes\": %d,\n", summary.bytes))
+   io.write(string.format("\t\"requests_per_sec\": %0.2f,\n", (summary.requests/summary.duration)*1e6))
+   io.write(string.format("\t\"bytes_transfer_per_sec\": %0.2f,\n", (summary.bytes/summary.duration)*1e6))
+
+   io.write("\t\"latency_distribution\": [\n")
+   for _, p in pairs({ 50, 75, 90, 99, 99.9, 99.99, 99.999, 100 }) do
+      io.write("\t\t{\n")
+      n = latency:percentile(p)
+      io.write(string.format("\t\t\t\"percentile\": %g,\n\t\t\t\"latency_in_microseconds\": %d\n", p, n))
+      if p == 100 then 
+          io.write("\t\t}\n")
+      else 
+          io.write("\t\t},\n")
+      end
+   end
+   io.write("\t]\n}\n")
+end
