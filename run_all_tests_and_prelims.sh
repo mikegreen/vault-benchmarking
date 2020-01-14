f8d52ed354f4ca784c528bc6bc97453a4cddb6b5
Mike Green <772413+mikegreen@users.noreply.github.com>
2020-01-13 18:12:30 -0700

    doc update and add helpful comments to run script

--- run_all_tests_and_prelims.sh
+++ run_all_tests_and_prelims.sh
@@ -16,44 +16,57 @@ wrk -t1 -c1 -d1m -H "X-Vault-Token: $VAULT_TOKEN" -s write-list.lua $VAULT_ADDR 
 # Run read test in background
 # Make sure that the secrets already exist in Vault before running this test
 # You can use write-secrets.lua (after some modification) to populate them
+echo "Launch read-secrets.lua"
 nohup wrk -t4 -c16 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s read-secrets.lua $VAULT_ADDR -- 1000 false > $TIME/prod-test-read-1000-random-secrets-t4-c16-6hours.log &
 
 # Run list test in background
 # Make sure that the secrets already exist in Vault before running this test
 # You can use write-secrets.lua (after some modification) to populate them
+echo "Launch list-secrets.lua"
 nohup wrk -t1 -c2 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s list-secrets.lua $VAULT_ADDR -- false > $TIME/prod-test-list-100-secrets-t1-c2-6hours.log &
 
 # Run authentication/revocation test in background
+echo "Launch authenticate-and-revoke.lua"
 nohup wrk -t1 -c16 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s authenticate-and-revoke.lua $VAULT_ADDR > $TIME/prod-test-authenticate-revoke-t1-c16-6hours.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 1"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 1 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test1.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 2"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 2 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test2.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 3"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 3 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test3.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 4"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 4 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test4.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 5"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 5 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test5.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 6"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 6 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test6.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 7"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 7 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test7.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 8"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 8 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test8.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 9"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 9 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test9.log &
 
 # Run write/delete test in background
+echo "Launch write-delete-secrets.lua thread 10"
 nohup wrk -t1 -c1 -d$DURATION -H "X-Vault-Token: $VAULT_TOKEN" -s write-delete-secrets.lua $VAULT_ADDR -- 10 100 > $TIME/prod-test-write-and-delete-100-secrets-t1-c1-6hours-test10.log &
 
 sleep $DURATION && sleep 30s
