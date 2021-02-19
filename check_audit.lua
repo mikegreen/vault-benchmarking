13f36cd2f7e26a9b1a6e75fcb462508254e10f94
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-02-19 14:52:07 -0700

    updates for transit

--- check_audit.lua
+++ check_audit.lua
@@ -2,11 +2,14 @@ function check_audit()
         local file = assert(io.popen('vault audit list', 'r'))
         local audit_output = file:read('*all')
         file:close()
---        print("Audit list says: " .. audit_output) -- > Prints the output of the command.
+        print("Audit list says: " .. audit_output) -- > Prints the output of the command.
 
         if string.match(audit_output, "No audit devices are enabled.") then
                 print "Auditing is disabled"
 		audit_enabled = false
+        elseif string.match(audit_output, "") then
+                print "Checking audit returned nothing, unsure if audit is enabled."
+                audit_enabled = false
         else
                 print "Audit is enabled. Eventually this should tell you which audit method as well."
 		audit_enabled = true
